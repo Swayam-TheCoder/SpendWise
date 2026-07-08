4f42d0b356869f450ccde26561cc5d33282ca1d2
Swayam-The Coder <swayamcode1@gmail.com>
2026-07-08 16:12:53 +0530

    api testing

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const { success } = require('zod');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// Middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(helmet());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// Health route check
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Expense Tracker API is running"
+  })
+})
 
 const PORT = process.env.PORT || 5000;
 
