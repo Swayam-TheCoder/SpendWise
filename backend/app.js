ed8d047e4ce739f4afe2679d8af792caff109419
Swayam-The Coder <swayamcode1@gmail.com>
2026-08-16 19:34:39 +0530

    db is working

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,15 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-
-dotenv.config();
+import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-app.use(helmet());
 app.use(cookieParser());
-app.use(morgan("dev"));
 
-// Health route check
-app.get("/api", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Expense Tracker API is running"
-  })
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success:true, message: "SpendWise API is running!" })
 })
 
-module.exports = app;
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
-const app = require('./app');
-const connectDB = require('./config/db');
-connectDB();
+dotenv.config();
+import dotenv from "dotenv";
+import app from "./app.js";
+import prisma from "./config/prisma.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on the PORT ${PORT}`);
-})
+const startServer = async () => {
+  try{
+    await prisma.$connect();
+    console.log("PostgreSQL database connected successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }
+  catch(err){
+    console.error("Error connecting to the database", err);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+
+
+
+
+
+
 
 // app.js -> responsible for creating express app, middlewares, routes, error handling
 // server.js -> responsible for loading env, Connecting to the database, starting the server
